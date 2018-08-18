d706ca6ad41ca68673921d6475308c4be283052b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-18 10:47:18 -0700

    Forloops

---
+++ src/extratwo/Forloops.java
@@ -0,0 +1,66 @@
+package extratwo;
+
+public class Forloops {
+public static void main(String[] args) {
+	
+//1
+	for(int i=0;i<=100;i++) {
+	System.out.println(i);
+	}
+	
+//2	
+	for(int i=100;i>=0;i--) {
+		System.out.println(i);
+	}
+	
+//3
+	for(int i=2;i%2==0;i++);
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+}
